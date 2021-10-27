34d69f33bb2c1c854315b27b564953ce1076c329
Gianluca <gianluca.ansaldi.1989@gmail.com>
2021-10-27 13:38:01 +0100

    refactor of _printBody() in Statement class

--- src/statement.js
+++ src/statement.js
@@ -10,20 +10,21 @@ class Statement {
 
   _printBody(transactions) {
     transactions.reverse().map((transaction) => {
-      if (transaction[1] == 'deposit') {
-        console.log(
-          `${transaction[0]} || ${transaction[2].toFixed(
-            2
-          )} || || ${transaction[3].toFixed(2)}`
-        );
-      } else {
-        console.log(
-          `${transaction[0]} || || ${transaction[2].toFixed(
-            2
-          )} || ${transaction[3].toFixed(2)}`
-        );
-      }
+      console.log(this._formatTransactions(transaction));
     });
+  }
+
+  _formatTransactions(transaction) {
+    if (transaction[1] == 'deposit') {
+      return `${transaction[0]} || ${transaction[2].toFixed(
+        2
+      )} || || ${transaction[3].toFixed(2)}`;
+    }
+    if (transaction[1] == 'withdrawal') {
+      return `${transaction[0]} || || ${transaction[2].toFixed(
+        2
+      )} || ${transaction[3].toFixed(2)}`;
+    }
   }
 }
 
